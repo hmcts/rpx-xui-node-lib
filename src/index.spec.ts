3b3873a1ae81d76268c1d06dd76782f1f727d3b2
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-29 21:53:50 +0100

    test(auth): fixed failing unit test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,10 @@
-import { auth } from './index';
+import { xuiNodeLib } from './index';
+import { Request, Response } from 'express';
 
 test('Auth', () => {
-    expect(auth('Carl')).toBe('Hello Carl');
+  const req = ({} as unknown) as Request;
+  const res = ({} as unknown) as Response;
+    const nextMock = jest.fn();
+    xuiNodeLib({})(req, res, nextMock);
+    expect(nextMock.mock.calls.length).toBe(1);
 });
