3e4bffcbf598e8b7b03a104742a348a0a74d3e35
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-29 21:45:32 +0100

    style: fixed linting issues

--- src/auth/oidc.strategy.ts
+++ src/auth/oidc.strategy.ts
@@ -1,21 +1,21 @@
-import { Issuer, Strategy } from "openid-client";
+import { Issuer, Strategy } from 'openid-client';
 
-let clientMetadata = {
+const clientMetadata = {
     client_id: 'xuiwebapp',
     client_secret: '***REMOVED***',
     // token_endpoint_auth_method: 'client_secret_post', // The default is 'client_secret_basic'.
+    post_logout_redirect_uri: 'http://localhost:3000',
     redirect_uri: 'http://localhost:3000/oauth2/callback',
-    post_logout_redirect_uri: 'http://localhost:3000'
-}
+};
 
 const strategy = (async () => {
-  const issuer = await Issuer.discover('https://idam-api.demo.platform.hmcts.net/o');
-  const client = new issuer.Client(clientMetadata);
-  // @ts-ignore
-   new Strategy({ client }, (tokenset: any, userinfo: any, done: any) => {
-    console.log(tokenset, userinfo);
-    done(null, userinfo);
-  });
+    const issuer = await Issuer.discover('https://idam-api.demo.platform.hmcts.net/o');
+    const client = new issuer.Client(clientMetadata);
+    // @ts-ignore
+    return new Strategy({ client }, (tokenset: any, userinfo: any, done: any) => {
+        // console.log(tokenset, userinfo);
+        done(null, userinfo);
+    });
 })();
 
 export default strategy;
