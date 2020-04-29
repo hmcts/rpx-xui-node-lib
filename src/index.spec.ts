d8a319fc36b542ee9389e57726b3e370fd31c551
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-29 21:57:20 +0100

    style(auth): prettier formatting

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import { xuiNodeLib } from './index';
 
 test('Auth', () => {
-  const req = ({} as unknown) as Request;
-  const res = ({} as unknown) as Response;
+    const req = ({} as unknown) as Request;
+    const res = ({} as unknown) as Response;
     const nextMock = jest.fn();
     xuiNodeLib({})(req, res, nextMock);
     expect(nextMock.mock.calls.length).toBe(1);
