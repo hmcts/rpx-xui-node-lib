e7f256ca0ecf744cff2a469e8d262a03267a519a
Pardeep Singh Basi <paddybasi@gmail.com>
2020-04-30 17:09:18 +0100

    style: commit hooks reworked and code format

--- src/auth/interfaces/clientMetadata.interface.ts
+++ src/auth/interfaces/clientMetadata.interface.ts
@@ -1,15 +1,12 @@
-
 export interface IClientMetadata {
-  clientID: string;
-  clientSecret: string;
-  redirectUrl: string;
+    clientID: string;
+    clientSecret: string;
+    redirectUrl: string;
 
+    // token_endpoint_auth_method: 'client_secret_post', // The default is 'client_secret_basic'.
+    post_logout_redirect_uri: 'http://localhost:3000';
 
-  // token_endpoint_auth_method: 'client_secret_post', // The default is 'client_secret_basic'.
-  post_logout_redirect_uri: 'http://localhost:3000',
-
-
-  // oauth2
-  authorizationURL: 'https://www.example.com/oauth2/authorize',
-  tokenURL: 'https://www.example.com/oauth2/token',
+    // oauth2
+    authorizationURL: 'https://www.example.com/oauth2/authorize';
+    tokenURL: 'https://www.example.com/oauth2/token';
 }
