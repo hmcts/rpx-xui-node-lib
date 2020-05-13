3319a1bb2f38c6eb73afcb80e59d697ff337ab4e
Pardeep Singh Basi <paddybasi@gmail.com>
2020-05-13 12:09:34 +0100

    style(style): changed library code so we don't have to use semi-colons

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        'no-extra-semi': "error",
+
     }
 };
