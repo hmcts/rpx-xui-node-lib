d56d2c2c84742acc88d22421e47fc4bd913aa2c7
Uday Denduluri <uday.denduluri@gmail.com>
2020-05-15 14:58:23 +0100

    refactor(oidc): adding validation and switching it on now

--- src/auth/oidc/validation/openIdOptions.Validation.ts
+++ src/auth/oidc/validation/openIdOptions.Validation.ts
@@ -1,14 +1,21 @@
 import { OpenIDMetadata } from '../OpenIDMetadata'
 import Joi, { ValidationResult } from '@hapi/joi'
+import { ResponseType } from 'openid-client'
 
 export function ValidateOpenIdOptions(options: OpenIDMetadata) {
     /* eslint-disable @typescript-eslint/camelcase */
     const schema = Joi.object({
-        discovery_endpoint: Joi.required(),
-        issuer_url: Joi.required(),
-        redirect_uri: Joi.required(),
-        scope: Joi.required(),
-        logout_url: Joi.required(),
+        client_id: Joi.string().required(),
+        client_secret: Joi.string().required(),
+        discovery_endpoint: Joi.string().required(),
+        issuer_url: Joi.string().required(),
+        logout_url: Joi.string().required(),
+        redirect_uri: Joi.string().required(),
+        response_types: Joi.array().required().min(1),
+        scope: Joi.string().required(),
+        sessionKey: Joi.string().required(),
+        token_endpoint_auth_method: Joi.string().required(),
+        useRoutes: Joi.any(),
     })
     /* eslint-enable @typescript-eslint/camelcase */
     const { error, value } = schema.validate(options)
