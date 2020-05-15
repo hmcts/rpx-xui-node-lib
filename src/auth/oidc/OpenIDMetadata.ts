d86aecd76dd39d905ed0c5e453056e1f8283679f
Uday Denduluri <uday.denduluri@gmail.com>
2020-05-15 12:24:08 +0100

    feat(validation): validate openId

---
+++ src/auth/oidc/OpenIDMetadata.ts
@@ -0,0 +1,12 @@
+import { ClientMetadata } from 'openid-client'
+
+export interface OpenIDMetadata extends ClientMetadata {
+    discovery_endpoint: string
+    issuer_url: string
+    prompt?: 'login'
+    redirect_uri: string
+    scope: string
+    sessionKey?: string
+    isAuthRouteName?: string
+    logout_url: string
+}
