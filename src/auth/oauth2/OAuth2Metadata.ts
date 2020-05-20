f7961ff348414ad7db5adb2f9cdb134ed1d26d7e
Uday Denduluri <uday.denduluri@gmail.com>
2020-05-20 11:23:15 +0100

    refactor(oauth2): removing scope and sessionKey

--- src/auth/oauth2/OAuth2Metadata.ts
+++ src/auth/oauth2/OAuth2Metadata.ts
@@ -1,8 +1,6 @@
 import { _StrategyOptionsBase } from 'passport-oauth2'
 
 export interface OAuth2Metadata extends _StrategyOptionsBase {
-    scope: string
-    sessionKey?: string
     logoutUrl: string
     useRoutes?: boolean
 }
