55e7825e479971a4184530347acc0332cc4c6540
Pardeep Singh Basi <paddybasi@gmail.com>
2020-05-22 18:13:50 +0100

    fix(release): fixed npm publish

--- release.config.js
+++ release.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     branches: ['master'],
-    // plugins: [
-    //     '@semantic-release/commit-analyzer',
-    //     '@semantic-release/release-notes-generator',
-    //     '@semantic-release/changelog',
-    //     '@semantic-release/npm',
-    //     [
-    //         '@semantic-release/git',
-    //         {
-    //             assets: ['package.json'],
-    //             message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-    //         },
-    //     ],
-    // ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
 }
