1c28990deb0d9149650563a7b47f4328056a20c6
Pardeep Singh Basi <paddybasi@gmail.com>
2020-05-26 14:33:16 +0100

    build(changelog): added changelog.md to commit back

--- release.config.js
+++ release.config.js
@@ -8,7 +8,7 @@ module.exports = {
         [
             '@semantic-release/git',
             {
-                assets: ['package.json'],
+                assets: ['CHANGELOG.md', 'package.json'],
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
