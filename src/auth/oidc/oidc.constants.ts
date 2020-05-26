f2e65c4b54341578dee3692e655044a4f32376e3
Uday Denduluri <uday.denduluri@gmail.com>
2020-05-26 13:39:37 +0100

    refactor(auth): oidc and oAuth2 fix after re-factor

--- src/auth/oidc/oidc.constants.ts
+++ src/auth/oidc/oidc.constants.ts
@@ -1,8 +1,4 @@
 export const OIDC = {
-    EVENT: {
-        AUTHENTICATE_SUCCESS: 'oidc.authenticate.success',
-        AUTHENTICATE_FAILURE: 'oidc.authenticate.failure',
-    },
     PROMPT: 'login',
     STRATEGY_NAME: 'oidc',
 }
