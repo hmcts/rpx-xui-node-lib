c10cc5213957f269089b600ce77d82819aa94eb3
ernestman28 <ernest_man28@hotmail.com>
2020-05-29 11:14:00 +0100

    feat(pipeline): test semantic-release/git plugin

--- release.config.js
+++ release.config.js
@@ -11,5 +11,12 @@ module.exports = {
                 assets: ['CHANGELOG.md', 'package.json'],
             },
         ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
     ],
 }
