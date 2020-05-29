ad6c38ea5db14f548f3d622fa29295494d242dcb
Pardeep Singh Basi <paddybasi@gmail.com>
2020-05-29 13:25:35 +0100

    test(coverage): ensured coverage reports can be generated

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    displayName: {
+        name: 'XUI Node Lib',
+        color: 'magenta',
+    },
+    transform: {
+        '^.+\\.(t|j)sx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
+}
