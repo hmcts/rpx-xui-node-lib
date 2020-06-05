a11ba7541c71dfd56896121811c14c1f70516f9c
Uday Denduluri <uday.denduluri@gmail.com>
2020-06-05 10:13:45 +0100

    test(auth): unit test for getOpenIDOptions

--- src/auth/oidc/models/openid.class.spec.ts
+++ src/auth/oidc/models/openid.class.spec.ts
@@ -62,3 +62,34 @@ test('OIDC configure initializeSession', () => {
     oidc.initializeSession()
     expect(spy).toBeCalled()
 })
+
+test('OIDC OptionsMapper', () => {
+    const options = {
+        authorizationURL: '',
+        tokenURL: '',
+        clientID: 'clientId',
+        clientSecret: 'Clientsecret',
+        discoveryEndpoint: 'someEndpoint',
+        issuerURL: 'issuer_url',
+        logoutURL: 'logouturl',
+        callbackURL: 'redirect_uri',
+        responseTypes: ['none'],
+        scope: 'some scope',
+        sessionKey: 'key',
+        tokenEndpointAuthMethod: 'client_secret_basic',
+        useRoutes: false,
+    }
+    const openIdOptions = oidc.getOpenIDOptions(options)
+
+    expect(openIdOptions.client_id).toEqual(options.clientID)
+    expect(openIdOptions.client_secret).toEqual(options.clientSecret)
+    expect(openIdOptions.discovery_endpoint).toEqual(options.discoveryEndpoint)
+    expect(openIdOptions.issuer_url).toEqual(options.issuerURL)
+    expect(openIdOptions.logout_url).toEqual(options.logoutURL)
+    expect(openIdOptions.redirect_uri).toEqual(options.callbackURL)
+    expect(openIdOptions.response_types).toEqual(options.responseTypes)
+    expect(openIdOptions.scope).toEqual(options.scope)
+    expect(openIdOptions.sessionKey).toEqual(options.sessionKey)
+    expect(openIdOptions.token_endpoint_auth_method).toEqual(options.tokenEndpointAuthMethod)
+    expect(openIdOptions.useRoutes).toEqual(options.useRoutes)
+})
