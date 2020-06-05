2e39c36a49fffc5c982cb0e34fe3d681802f4d59
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2020-06-05 14:34:43 +0100

    Feature/auth unit test (#29)
    * test(auth): adding unit tests for openId and minor refactor to methods
    
    * test(auth): openId test
    
    * test(auth): loginHandler unit test
    
    * test(auth): unit test for getOpenIDOptions
    
    * test(auth): adding tests for validateOptions
    
    * test(auth): unit test
    
    * test(auth): unit test for verifyLogin events
    
    * test(unit test): remove try catch and use expect
    
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- src/auth/oidc/models/openid.class.spec.ts
+++ src/auth/oidc/models/openid.class.spec.ts
@@ -1,6 +1,7 @@
 import oidc from './openid.class'
 import passport from 'passport'
 import { Request, Response, NextFunction } from 'express'
+import { AUTH } from '../../auth.constants'
 
 test('OIDC Auth', () => {
     expect(oidc).toBeDefined()
@@ -61,4 +62,116 @@ test('OIDC configure initializeSession', () => {
     const spy = jest.spyOn(passport, 'session')
     oidc.initializeSession()
     expect(spy).toBeCalled()
+})
+
+test('OIDC OptionsMapper', () => {
+    const options = {
+        authorizationURL: '',
+        tokenURL: '',
+        clientID: 'clientId',
+        clientSecret: 'Clientsecret',
+        discoveryEndpoint: 'someEndpoint',
+        issuerURL: 'issuer_url',
+        logoutURL: 'logouturl',
+        callbackURL: 'redirect_uri',
+        responseTypes: ['none'],
+        scope: 'some scope',
+        sessionKey: 'key',
+        tokenEndpointAuthMethod: 'client_secret_basic',
+        useRoutes: false,
+    }
+    const openIdOptions = oidc.getOpenIDOptions(options)
+
+    expect(openIdOptions.client_id).toEqual(options.clientID)
+    expect(openIdOptions.client_secret).toEqual(options.clientSecret)
+    expect(openIdOptions.discovery_endpoint).toEqual(options.discoveryEndpoint)
+    expect(openIdOptions.issuer_url).toEqual(options.issuerURL)
+    expect(openIdOptions.logout_url).toEqual(options.logoutURL)
+    expect(openIdOptions.redirect_uri).toEqual(options.callbackURL)
+    expect(openIdOptions.response_types).toEqual(options.responseTypes)
+    expect(openIdOptions.scope).toEqual(options.scope)
+    expect(openIdOptions.sessionKey).toEqual(options.sessionKey)
+    expect(openIdOptions.token_endpoint_auth_method).toEqual(options.tokenEndpointAuthMethod)
+    expect(openIdOptions.useRoutes).toEqual(options.useRoutes)
+})
+
+test('test validateOptions', () => {
+    const options = {
+        authorizationURL: '',
+        tokenURL: '454',
+        clientID: 'clientId',
+        clientSecret: 'Clientsecret',
+        discoveryEndpoint: 'someEndpoint',
+        issuerURL: 'issuer_url',
+        logoutURL: 'logouturl',
+        callbackURL: 'redirect_uri',
+        responseTypes: ['none'],
+        scope: 'some scope',
+        sessionKey: 'key',
+        tokenEndpointAuthMethod: 'client_secret_basic',
+        useRoutes: false,
+    }
+    expect(() => {
+        oidc.validateOptions(options)
+    }).toThrowError('authorizationURL')
+
+    //positive case
+    options.authorizationURL = 'something'
+    const isValid = oidc.validateOptions(options)
+    expect(isValid).toBeTruthy()
+})
+test('OIDC verifyLogin error Path', () => {
+    const mockRequest = {
+        body: {},
+    } as Request
+    mockRequest.logIn = (user: any, done: (err: any) => void) => {
+        console.log('mockRequest.logIn')
+        done({})
+    }
+    const mockResponse = {} as Response
+    const next = jest.fn()
+    const user = {}
+
+    oidc.verifyLogin(mockRequest, user, next, mockResponse)
+    expect(next).toBeCalledWith({})
+})
+
+test('OIDC verifyLogin happy Path with no subscription', () => {
+    const mockRequest = {
+        body: {},
+    } as Request
+    mockRequest.logIn = (user: any, done: (err: any) => void) => {
+        console.log('mockRequest.logIn')
+        done(undefined)
+    }
+    const mockResponse = {} as Response
+    const mockRedirect = jest.fn()
+    mockResponse.redirect = mockRedirect
+    const next = jest.fn()
+    const user = {}
+
+    oidc.verifyLogin(mockRequest, user, next, mockResponse)
+    expect(next).not.toBeCalledWith({})
+    expect(mockRedirect).toBeCalledWith(AUTH.ROUTE.DEFAULT_REDIRECT)
+})
+
+test('OIDC verifyLogin happy Path with subscribtion', () => {
+    const mockRequest = {
+        body: {},
+    } as Request
+    mockRequest.logIn = (user: any, done: (err: any) => void) => {
+        console.log('mockRequest.logIn')
+        done(undefined)
+    }
+    const mockResponse = {} as Response
+    const mockRedirect = jest.fn()
+    mockResponse.redirect = mockRedirect
+    const next = jest.fn()
+    const user = {}
+
+    oidc.addListener(AUTH.EVENT.AUTHENTICATE_SUCCESS, (authObject, isVerify) => {
+        expect(isVerify).toBeFalsy()
+    })
+    oidc.verifyLogin(mockRequest, user, next, mockResponse)
+    expect(next).not.toBeCalledWith({})
 })
