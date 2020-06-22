8ffe04bf63ca38dbbc5bafe0e8fe6020bb43c9a8
Pardeep Singh Basi <paddybasi@gmail.com>
2020-06-22 19:37:58 +0100

    fix(code smell): tidy up

--- src/common/util/debug.logger.ts
+++ src/common/util/debug.logger.ts
@@ -7,8 +7,8 @@ const debugLogger = debug('xuiNode')
 export const logFn = (...args: any[]): any => {
     const calledPath = callerPath()
     const layers = calledPath.split(path.sep).slice(0, 2).join(':')
-    const logger = debugLogger.extend(layers)
-    logger(args)
+    const loggerFn = debugLogger.extend(layers)
+    loggerFn(args)
 }
 
 //TODO: see if we can control colours per method instead of a generic function
