426274d927372430fc059206ed68d47446b61077
udaydenduluri33 <50869558+udaydenduluri33@users.noreply.github.com>
2021-03-09 10:04:27 +0000

    refactor(strategy.class.ts): adding log instead of info (#126)
    Co-authored-by: Uday Denduluri <uday.denduluri@gmail.com>

--- src/auth/models/strategy.class.ts
+++ src/auth/models/strategy.class.ts
@@ -276,11 +276,11 @@ export abstract class Strategy extends events.EventEmitter {
         let routeCredentialToken
         const cachedToken = req.app.get('routeCredentialToken')
         if (cachedToken && cachedToken.access_token && !this.isTokenExpired(cachedToken.access_token)) {
-            this.logger.info('using cached routeCredentialToken')
+            this.logger.log('using cached routeCredentialToken')
             routeCredentialToken = cachedToken
         } else {
             routeCredentialToken = await this.generateToken()
-            this.logger.info('using new routeCredentialToken')
+            this.logger.log('using new routeCredentialToken')
             req.app.set('routeCredentialToken', routeCredentialToken)
         }
         if (routeCredentialToken && routeCredentialToken.access_token) {
