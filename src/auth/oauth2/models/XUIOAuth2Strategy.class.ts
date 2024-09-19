fe0100e04b58f32eb170734ca34d030cf8b1d68e
Andy Wilkins <andy.wilkins@hmcts.net>
2024-09-19 16:02:05 +0100

    bit more logging

--- src/auth/oauth2/models/XUIOAuth2Strategy.class.ts
+++ src/auth/oauth2/models/XUIOAuth2Strategy.class.ts
@@ -3,17 +3,26 @@
 import { OAuth2Metadata } from './OAuth2Metadata.interface'
 import OAuth2Strategy from 'passport-oauth2'
 import { AxiosResponse } from 'axios'
-import { http } from '../../../common'
+import { getLogger, http, XuiLogger } from '../../../common'
 
 export class XUIOAuth2Strategy extends OAuth2Strategy {
     private readonly options: OAuth2Metadata
+    private readonly logger: XuiLogger = getLogger('auth:XUIOAuth2Strategy')
+
     constructor(options: OAuth2Metadata, verify: OAuth2Strategy.VerifyFunction) {
         super(options, verify)
         this.options = options
     }
     userProfile = async (accessToken: string, done: (err?: Error | null, profile?: any) => void): Promise<void> => {
         const userDetails = await getUserDetails(accessToken, this.options.logoutUrl)
-        done(null, userDetails.data)
+        if (userDetails?.data) {
+            this.logger.log('retrieved userProfile', userDetails.data);
+            done(null, userDetails.data)
+        } else {
+            this.logger.error('no user details retrieved')
+            const e: Error = new Error('XUIOAuth2Strategy no user details retrieved')
+            done(e, null)
+        }
     }
 }
 
