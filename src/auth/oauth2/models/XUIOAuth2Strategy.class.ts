3b30e0dc0e4a3ccabd1868381650ada312518146
Andy Wilkins <andy.wilkins@hmcts.net>
2024-09-19 16:19:31 +0100

    Stupid linting

--- src/auth/oauth2/models/XUIOAuth2Strategy.class.ts
+++ src/auth/oauth2/models/XUIOAuth2Strategy.class.ts
@@ -16,7 +16,7 @@ export class XUIOAuth2Strategy extends OAuth2Strategy {
     userProfile = async (accessToken: string, done: (err?: Error | null, profile?: any) => void): Promise<void> => {
         const userDetails = await getUserDetails(accessToken, this.options.logoutUrl)
         if (userDetails?.data) {
-            this.logger.log('retrieved userProfile', userDetails.data);
+            this.logger.log('retrieved userProfile', userDetails.data)
             done(null, userDetails.data)
         } else {
             this.logger.error('no user details retrieved')
