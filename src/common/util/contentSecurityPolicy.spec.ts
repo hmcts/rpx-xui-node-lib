682c72d1dac9e83b45fd3f3153e09fc58c628bda
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2024-11-19 15:41:01 +0000

    Add test for Sonar

---
+++ src/common/util/contentSecurityPolicy.spec.ts
@@ -0,0 +1,11 @@
+import { getContentSecurityPolicy } from './'
+import { SECURITY_POLICY } from './contentSecurityPolicy';
+/**
+ * Same as isStringPatternMatch() but testing with multiple strings
+ */
+describe('getContentSecurityPolicy(helmet)', () => {
+  it("should correctly call the content security policy", () => {
+    const helmet: any = {contentSecurityPolicy: (policy: any) => {return policy}}
+    expect(getContentSecurityPolicy(helmet)).toBe(SECURITY_POLICY)
+  });
+})
