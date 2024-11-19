3c46d19140cb747ec2ee1fa58a8c3e09ce534dd6
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2024-11-19 17:07:00 +0000

    Add test changes

--- src/common/util/contentSecurityPolicy.spec.ts
+++ src/common/util/contentSecurityPolicy.spec.ts
@@ -1,11 +1,15 @@
 import { getContentSecurityPolicy } from './'
-import { SECURITY_POLICY } from './contentSecurityPolicy';
+import { SECURITY_POLICY } from './contentSecurityPolicy'
 /**
  * Same as isStringPatternMatch() but testing with multiple strings
  */
 describe('getContentSecurityPolicy(helmet)', () => {
-  it("should correctly call the content security policy", () => {
-    const helmet: any = {contentSecurityPolicy: (policy: any) => {return policy}}
-    expect(getContentSecurityPolicy(helmet)).toBe(SECURITY_POLICY)
-  });
+    it('should correctly call the content security policy', () => {
+        const helmet: any = {
+            contentSecurityPolicy: (policy: any) => {
+                return policy
+            },
+        }
+        expect(getContentSecurityPolicy(helmet)).toBe(SECURITY_POLICY)
+    })
 })
