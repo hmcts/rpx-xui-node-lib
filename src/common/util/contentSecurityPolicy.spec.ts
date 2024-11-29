acf0e26f75662f056ee1c799cac71ca1eea96a0b
connorpgpmcelroy <74015088+connorpgpmcelroy@users.noreply.github.com>
2024-11-29 09:26:31 +0000

    Update contentSecurityPolicy.spec.ts
    Remove unhelpful comment

--- src/common/util/contentSecurityPolicy.spec.ts
+++ src/common/util/contentSecurityPolicy.spec.ts
@@ -1,8 +1,6 @@
 import { getContentSecurityPolicy } from './'
 import { SECURITY_POLICY } from './contentSecurityPolicy'
-/**
- * Same as isStringPatternMatch() but testing with multiple strings
- */
+
 describe('getContentSecurityPolicy(helmet)', () => {
     it('should correctly call the content security policy', () => {
         const helmet: any = {
