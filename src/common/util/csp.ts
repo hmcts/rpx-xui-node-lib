fe11e4318f5a0e354d574a87acfaec9c455b0c85
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-07-31 15:25:11 +0100

    Make additional changes to include old csp as standard

--- src/common/util/csp.ts
+++ src/common/util/csp.ts
@@ -7,7 +7,9 @@ export function csp({
   extraStyle   = (process.env.CSP_STYLE_EXTRA   ?? '').split(',').filter(Boolean),
   extraConnect = (process.env.CSP_CONNECT_EXTRA ?? '').split(',').filter(Boolean),
   extraFont    = (process.env.CSP_FONT_EXTRA    ?? '').split(',').filter(Boolean),
-  extraImg     = (process.env.CSP_IMG_EXTRA     ?? '').split(',').filter(Boolean)
+  extraImg     = (process.env.CSP_IMG_EXTRA     ?? '').split(',').filter(Boolean),
+  // old csp to merge with
+  defaultCsp   = {}
 } = {}) {
   return (
     req: Request,
@@ -16,8 +18,7 @@ export function csp({
   ) => {
     const nonce: string = crypto.randomBytes(16).toString('base64');
     res.locals.cspNonce = nonce;
-
-    helmet.contentSecurityPolicy({
+    const newCsp = {
       useDefaults: true,
       directives: {
         "default-src": ["'self'"],
@@ -31,7 +32,11 @@ export function csp({
         "frame-ancestors": ["'self'"],
         "form-action": ["'none'"]
       },
+      // if CSP_REPORT_ONLY is set to true, use report-only mode
+      // this allows you to test your CSP without enforcing it
       reportOnly: process.env.CSP_REPORT_ONLY === 'true'
-    })(req, res, next);
+    }
+    const csp = { ...defaultCsp, ...newCsp };
+    helmet.contentSecurityPolicy(csp)(req, res, next);
   };
 }
