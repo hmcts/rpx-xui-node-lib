55ae699a0b6ef6726c906fab6eac9df3dd04349c
connorpgpmcelroy <connor.mcelroy@hmcts.net>
2025-08-01 16:58:38 +0100

    Fix csp merging

--- src/common/util/csp.ts
+++ src/common/util/csp.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import helmet from 'helmet';
 import crypto from 'node:crypto';
+import merge from 'deepmerge';                     // <-- deep merge helper
 
 export function csp({
   extraScript  = (process.env.CSP_SCRIPT_EXTRA  ?? '').split(',').filter(Boolean),
@@ -8,36 +9,37 @@ export function csp({
   extraConnect = (process.env.CSP_CONNECT_EXTRA ?? '').split(',').filter(Boolean),
   extraFont    = (process.env.CSP_FONT_EXTRA    ?? '').split(',').filter(Boolean),
   extraImg     = (process.env.CSP_IMG_EXTRA     ?? '').split(',').filter(Boolean),
-  // old csp to merge with
-  defaultCsp   = {}
+  defaultCsp = {}
 } = {}) {
-  return (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const nonce: string = crypto.randomBytes(16).toString('base64');
+  return (req: Request, res: Response, next: NextFunction) => {
+    const nonce = crypto.randomBytes(16).toString('base64');
     res.locals.cspNonce = nonce;
+
     const newCsp = {
       useDefaults: true,
       directives: {
         "default-src": ["'self'"],
         "script-src":  ["'self'", `'nonce-${nonce}'`, ...extraScript],
         "style-src":   ["'self'", `'nonce-${nonce}'`, ...extraStyle],
-        "styleSrcAttr":  ["'unsafe-inline'"],
+
+        /* allow inline style *attributes* (e.g. GOV-UK warning text) */
+        styleSrcAttr:  ["'unsafe-inline'"],
+
         "connect-src": ["'self'", "blob:", "data:", ...extraConnect],
         "img-src":     ["'self'", "data:", ...extraImg],
         "font-src":    ["'self'", "data:", "https://fonts.gstatic.com", ...extraFont],
+
         "object-src":  ["'none'"],
         "frame-src":   ["'self'"],
         "frame-ancestors": ["'self'"],
         "form-action": ["'none'"]
       },
-      // if CSP_REPORT_ONLY is set to true, use report-only mode
-      // this allows you to test your CSP without enforcing it
+      /** switch header to report-only mode if the env-var is set */
+      // CSP will not break application in report-only mode, but will log violations
       reportOnly: process.env.CSP_REPORT_ONLY === 'true'
-    }
-    const csp = { ...defaultCsp, ...newCsp };
-    helmet.contentSecurityPolicy(csp)(req, res, next);
+    };
+
+    const finalCsp = merge(defaultCsp, newCsp);
+    helmet.contentSecurityPolicy(finalCsp)(req, res, next);
   };
 }
