387165442c4d0000c67b52df86107753cf7fe47f
Olu <142989683+olusegz07@users.noreply.github.com>
2025-08-14 11:05:53 +0100

    Revert "EXUI-2541 - New branch pre passport (#271) (#273)"
    This reverts commit 3979c0666a32e6ce36d89615075280485598ffe5.

--- src/auth/models/strategy.class.ts
+++ src/auth/models/strategy.class.ts
@@ -511,7 +511,7 @@ export abstract class Strategy extends events.EventEmitter {
             })
             // cookie options added via EXUI-986, fortify issues
             const cookieOptions: CookieOptions = {
-                sameSite: 'strict',
+                sameSite: 'none',
                 secure: true,
             }
             /* istanbul ignore next */
