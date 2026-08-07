09ed93d0d19f6432f0b6647de131ac12fee78314
Olu <142989683+olusegz07@users.noreply.github.com>
2026-08-07 10:53:51 +0100

    Update openid.class.spec.ts

--- src/auth/oidc/models/openid.class.spec.ts
+++ src/auth/oidc/models/openid.class.spec.ts
@@ -185,7 +185,7 @@ test('OIDC jwTokenExpired', () => {
     let isTokenExpired = oidc.jwTokenExpired(jwtData)
     expect(isTokenExpired).toBeTruthy()
 
-    jwtData = { exp: Math.floor(Date.now() / 1000) + 60 }
+    jwtData = { exp: new Date().getTime() / 1000 }
     isTokenExpired = oidc.jwTokenExpired(jwtData)
     expect(isTokenExpired).toBeFalsy()
 })
