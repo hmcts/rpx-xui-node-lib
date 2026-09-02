0be09bd9d686dc8900fef1aa64c06f258a76bf4c
Olu <142989683+olusegz07@users.noreply.github.com>
2026-09-02 11:07:35 +0100

    chore: version update (#345)
    * version update
    
    * test: ignore build output and fix OIDC token expiry spec
    
    * Update jest.config.js
    
    * version update
    
    * Update openid.class.spec.ts
    
    * version update
    
    * elinst version change
    
    * cve update
    
    * bump up version
    
    ---------
    
    Co-authored-by: RiteshHMCTS <ritesh.dsouza@hmcts.net>

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ const config = {
         '^.+\\.(t|j)sx?$': ['ts-jest', { compiler: 'typescript' }],
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     restoreMocks: true,
     globals: {},
